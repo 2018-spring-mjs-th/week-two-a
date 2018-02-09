7169b26fba2cb2746b09b868d8047fbb04b2ad2f
tsteele <tsteele@madisoncollege.edu>
2018-02-09 15:05:22 -0600

    #week-03 "calculator"...
    And students did more of this ! ! !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,4 +39,13 @@ export class AppComponent {
   public deleteQuiz(quizToDelete) {
     this.quizzes = this.quizzes.filter((quiz) => quiz !== quizToDelete);
   }
+
+  // Our #week-03 "calculator"...
+  number1:number = 0;
+  number2:number = 0;
+  answer:number = 0;
+  add = () => {
+    window.alert('here');
+    this.answer = Number(this.number1) + Number(this.number2)
+  };
 }
