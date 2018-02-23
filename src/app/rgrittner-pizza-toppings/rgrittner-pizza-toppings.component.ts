e9cfa9a18e602c9d279fa137f04782b55c7601af
rgrittner <renee.grittner@gmail.com>
2018-02-22 20:24:41 -0600

    Added stuff to the component html

--- src/app/rgrittner-pizza-toppings/rgrittner-pizza-toppings.component.ts
+++ src/app/rgrittner-pizza-toppings/rgrittner-pizza-toppings.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-rgrittner-pizza-toppings',
+  selector: 'rgrittner-pizza-toppings',
   templateUrl: './rgrittner-pizza-toppings.component.html',
   styleUrls: ['./rgrittner-pizza-toppings.component.css']
 })
