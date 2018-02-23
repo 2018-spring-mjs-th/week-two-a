5a89e8b0cd634c4366b7e3f52e8ad3c5c7968d11
rbruinsma <tober@rocketmail.com>
2018-02-22 20:11:30 -0600

    ng generate component RbruinsmaPizzaToppings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { FormsModule } from '@angular/forms';
 import { QuestionTypesService } from './question-types.service';
 import { PizzaToppingsService } from './pizza-toppings.service';
 import { QuestionTypesComponent } from './question-types/question-types.component';
+import { RbruinsmaPizzaToppingsComponent } from './rbruinsma-pizza-toppings/rbruinsma-pizza-toppings.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionTypesComponent
+    QuestionTypesComponent,
+    RbruinsmaPizzaToppingsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/rbruinsma-pizza-toppings/rbruinsma-pizza-toppings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rbruinsma-pizza-toppings',
+  templateUrl: './rbruinsma-pizza-toppings.component.html',
+  styleUrls: ['./rbruinsma-pizza-toppings.component.css']
+})
+export class RbruinsmaPizzaToppingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
