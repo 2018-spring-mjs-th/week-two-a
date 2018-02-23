aa6dacf98c17bf8b5de15eeff2e790368d443d4e
Aaron Meyer <armeyer2@madisoncollege.edu>
2018-02-22 18:53:31 -0600

    Displaying qts to the page

--- src/app/question-types/question-types.component.ts
+++ src/app/question-types/question-types.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionTypesService } from '../question-types.service';
 
 @Component({
   selector: 'app-question-types',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionTypesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private qtService: QuestionTypesService) { }
+
+  public questTypes: string[];
 
   ngOnInit() {
+    this.questionTypes = this.qtService.getQuestionTypes();
   }
 
 }
