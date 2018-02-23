3169530e0de682e095430eba1daac2475cb2dffa
Amy Knapp <amyjknapp@gmail.com>
2018-02-22 19:09:55 -0600

    used functional programming to add a string array
    Uh...

---
+++ src/app/question-types/question-types.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionTypesService } from '../question-types.service';
+
+interface quizType {
+  name: string;
+  checked: boolean;
+}
+
+@Component({
+  selector: 'question-types',
+  templateUrl: './question-types.component.html',
+  styleUrls: ['./question-types.component.css']
+})
+export class QuestionTypesComponent implements OnInit {
+
+  constructor(private qtService: QuestionTypesService) { }
+
+  public questionTypes: quizType[];
+
+  ngOnInit() {
+    this.questionTypes = this.qtService.getQuestionTypes()
+      .map(x => ({ name: x, checked: false }));
+  }
+
+} 
+
+
