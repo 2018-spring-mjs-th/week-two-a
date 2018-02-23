921dafcf4fae6b06b4b3fd7da49fb61c175515a1
Aaron Meyer <armeyer2@madisoncollege.edu>
2018-02-22 19:08:48 -0600

    Changed qt to qt.name since it is an object

--- src/app/question-types/question-types.component.ts
+++ src/app/question-types/question-types.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionTypesService } from '../question-types.service';
 
+interface quizType{
+  name: string;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-question-types',
   templateUrl: './question-types.component.html',
@@ -10,10 +15,11 @@ export class QuestionTypesComponent implements OnInit {
 
   constructor(private qtService: QuestionTypesService) { }
 
-  public questTypes: string[];
+  public questTypes: quizType[];
 
   ngOnInit() {
-    this.questionTypes = this.qtService.getQuestionTypes();
+    this.questionTypes = this.qtService.getQuestionTypes()
+      .map(x => ({ name: x, checked: false}));
   }
 
 }
