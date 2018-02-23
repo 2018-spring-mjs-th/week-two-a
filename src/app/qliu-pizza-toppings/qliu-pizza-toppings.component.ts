96972e4d3e9604a41fc386c0e1c5e84957ffb60b
Qiao <31290063+Qiao0418@users.noreply.github.com>
2018-02-22 20:25:58 -0600

    Created a Component
    Add pizza component to root component

---
+++ src/app/qliu-pizza-toppings/qliu-pizza-toppings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qliu-pizza-toppings',
+  templateUrl: './qliu-pizza-toppings.component.html',
+  styleUrls: ['./qliu-pizza-toppings.component.css']
+})
+export class QliuPizzaToppingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
