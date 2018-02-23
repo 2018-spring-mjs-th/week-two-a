3988f30791ed1f5d59d551f059af43f8437857d4
rgrittner <renee.grittner@gmail.com>
2018-02-22 20:10:20 -0600

    created my component
    ng generate component RgrittnerPizzaToppings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { FormsModule } from '@angular/forms';
 import { QuestionTypesService } from './question-types.service';
 import { PizzaToppingsService } from './pizza-toppings.service';
 import { QuestionTypesComponent } from './question-types/question-types.component';
+import { RgrittnerPizzaToppingsComponent } from './rgrittner-pizza-toppings/rgrittner-pizza-toppings.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionTypesComponent
+    QuestionTypesComponent,
+    RgrittnerPizzaToppingsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/rgrittner-pizza-toppings/rgrittner-pizza-toppings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rgrittner-pizza-toppings',
+  templateUrl: './rgrittner-pizza-toppings.component.html',
+  styleUrls: ['./rgrittner-pizza-toppings.component.css']
+})
+export class RgrittnerPizzaToppingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
