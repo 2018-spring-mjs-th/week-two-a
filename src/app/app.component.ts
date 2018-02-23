82f507598ad0f971f7d72a88883611b3d49d660a
Qiao <31290063+Qiao0418@users.noreply.github.com>
2018-02-22 19:37:24 -0600

    Works!
    Yep

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { QuestionTypesComponent } from './question-types/question-types.component';
+
+
+interface quizDisplay {
+
+  name: string;
+  showDelete: boolean;
+  questionTypeSummary: string;
+}
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,6 +17,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild(QuestionTypesComponent)
+  public questionSubComponent: QuestionTypesComponent;
+
   title = 'week-two-a';
 
   // quizzes = [
@@ -18,25 +33,31 @@ export class AppComponent {
     this.quizzes = [{
         name: "Quiz 1"
         , showDelete: false
+        , questionTypeSummary: ""
       }
       , {
         name: "Quiz 3"
         , showDelete: false
+        , questionTypeSummary: ""
       }
     ];
   }
 
-  quizzes = [];
+  quizzes: quizDisplay[] = [];
 
   public addFunnyQuiz() {
     //this.quizzes.push("Funny Quiz");
-    this.quizzes.push({ name: "Funny Quiz", showDelete: false });
+    this.quizzes.push({ name: "Funny Quiz", showDelete: false, questionTypeSummary: "" });
   }
 
   newQuizName = "";
 
   public addQuiz() {
-    this.quizzes.push({ name: this.newQuizName, showDelete: true });
+
+    let foo = this.questionSubComponent.questionTypes.filter(x => x.checked).map(x => x.name).join(', ');
+
+
+    this.quizzes.push({ name: this.newQuizName, showDelete: true, questionTypeSummary: foo });
     this.newQuizName = "";
   }
 
