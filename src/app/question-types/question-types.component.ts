609705a088cc02775533884a3d625656f84a349e
tsteelematc <tsteele@madisoncollege.edu>
2018-02-22 18:53:22 -0600

    Got the service hooked up and template displaying qt's.

--- src/app/question-types/question-types.component.ts
+++ src/app/question-types/question-types.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionTypesService } from '../question-types.service';
 
 @Component({
   selector: 'question-types',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionTypesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private qtSvc: QuestionTypesService) { }
+
+  public questionTypes: string[];
 
   ngOnInit() {
+    this.questionTypes = this.qtSvc.getQuestionTypes();
   }
 
 }
