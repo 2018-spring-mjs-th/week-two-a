5ccf83a9165e420bf1b00279d6f1828f6e02418c
Amy Knapp <amyjknapp@gmail.com>
2018-02-23 20:10:33 -0600

    added constructor and interface
    Not really sure what I'm doing

--- src/app/aknapp-pizza-toppings/aknapp-pizza-toppings.component.ts
+++ src/app/aknapp-pizza-toppings/aknapp-pizza-toppings.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaToppingsService } from '../pizza-toppings.service';
+
+interface toppingType {
+  name: string;
+  checked: boolean;
+  iLoveStatement: string;
+}
 
 @Component({
-  selector: 'app-aknapp-pizza-toppings',
+  selector: 'aknapp-pizza-toppings',
   templateUrl: './aknapp-pizza-toppings.component.html',
   styleUrls: ['./aknapp-pizza-toppings.component.css']
 })
 export class AknappPizzaToppingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ptService: PizzaToppingsService) { }
+
+  public toppingTypes: quizType[];
 
   ngOnInit() {
+    this.toppingTypes = this.ptService.getAvailablePizzaToppings()
+    .map(x => ({ name: x, checked: false, iLoveStatement: ""}));
   }
 
 }
