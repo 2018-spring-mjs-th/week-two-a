1f514b842f6f21e6f37e41ab90779d35c07891f6
j-stoff <jstoffregen@madisoncollege.edu>
2018-02-25 16:54:49 -0600

    Updated component files

---
+++ src/app/j-stoff-component/j-stoff-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-j-stoff-component',
+  templateUrl: './j-stoff-component.component.html',
+  styleUrls: ['./j-stoff-component.component.css']
+})
+export class JStoffComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
