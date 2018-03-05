36afd66eef3edf5d19345c38b2379ca8e5f7cd44
HolliNiesen <holli.niesen@gmail.com>
2018-03-04 20:57:36 -0600

    Adding pizza toppings.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { FormsModule } from '@angular/forms';
 import { QuestionTypesService } from './question-types.service';
 import { PizzaToppingsService } from './pizza-toppings.service';
 import { QuestionTypesComponent } from './question-types/question-types.component';
+import { HniesenPizzaToppingsComponent } from './hniesen-pizza-toppings-types/hniesen-pizza-toppings.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionTypesComponent
+    QuestionTypesComponent,
+    HniesenPizzaToppingsComponent
   ],
   imports: [
     BrowserModule
