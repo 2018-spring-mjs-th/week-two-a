b55e4d9bbf129033810a1e19f27516212d8e950b
bciancio <Brandonciancio@gmail.com>
2018-03-08 18:43:02 -0600

    Now functional

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaToppingsService } from '../pizza-toppings.service';
+
+interface PizzaTopping {
+	name: string;
+	checked: boolean;
+}
+
+
 
 @Component({
-  selector: 'pizza-toppings',
-  templateUrl: './pizza-toppings.component.html',
-  styleUrls: ['./pizza-toppings.component.css']
+    selector: 'pizza-toppings',
+    templateUrl: './pizza-toppings.component.html',
+    styleUrls: ['./pizza-toppings.component.css']
 })
 export class PizzaToppingsComponent implements OnInit {
 
-  constructor() { }
+	// Grab the Pizza Service
+	constructor(private pizza_service: PizzaToppingsService) { }
 
-  ngOnInit() {
-  }
 
+	selected_toppings: string;
+
+	topping_options: PizzaTopping[];
+	
+	// On init reference the pizza service and convert toppings to PizzaTopping object
+	ngOnInit() {
+		this.topping_options = this.pizza_service.getAvailablePizzaToppings()
+			.map(x => ({ name: x, checked: false }));
+	}
+
+	clearAllToppings() {
+		this.topping_options = this.topping_options
+			.map(x => ({ ...x, checked: false }));
+	}
+	
+	selectAllToppings() {
+		this.topping_options = this.topping_options
+			.map(x => ({ ...x, checked: true }));
+	}
+
+	refreshSelectedToppings() {	
+		this.selected_toppings = this.topping_options
+			.filter(x => x.checked)
+			.map(x => x.name).join(', ');
+	}
 }
+
