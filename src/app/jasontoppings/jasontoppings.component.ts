bf7201665afeb459260e29d44d9bc3733d3525c4
Jason Matzke <jmatzke1@madisoncollege.edu>
2018-03-08 18:21:35 -0600

    added rest

--- src/app/jasontoppings/jasontoppings.component.ts
+++ src/app/jasontoppings/jasontoppings.component.ts
@@ -15,12 +15,39 @@ export class JasontoppingsComponent implements OnInit {
 
   public pizzaToppings: jmPizzaTopping [];
   public pizzaDescription: string;
+  public price: number;
   constructor(private pizzaService: PizzaService) { }
 
   ngOnInit() {
     this.pizzaToppings = this.pizzaService.getAvailablePizzaToppings()
         .map(x =>({name: x, checked: false}));
     this.pizzaDescription = "";
+    this.price = 10
   }
+public checkAllToppings() {
+  this.pizzaToppings.forEach(x => x.checked = true);
+}
+
+public removeAllToppings() {
+  this.pizzaToppings.forEach(x => x.checked = false);
+}
+
+public updateToppings() {
+  let numberOfSelectedTopping = this.pizzaToppings.filter(x=>x.checked ==true)
+
+  if(numberOfSelectedTopping.length === 0) {
+    this.pizzaDescription = "You have a plain pizza."
+  }
+  else {
+
+  this.pizzaDescription = "My pizza has ";
+  this.pizzaDescription += this.pizzaToppings
+          .filter(x => x.checked)
+          .map(x => x.name)
+          .join(', ');
+  
+  }
+  this.pizzaDescription += " \nIt will cost $" + this.price;
+}
 
 }
