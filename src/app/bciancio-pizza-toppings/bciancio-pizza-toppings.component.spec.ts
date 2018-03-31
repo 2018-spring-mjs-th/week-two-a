0ab572bef036e5121ecf8776c869647f8171f79e
bciancio <Brandonciancio@gmail.com>
2018-03-31 16:26:43 -0500

    Fix spacing for test class

--- src/app/bciancio-pizza-toppings/bciancio-pizza-toppings.component.spec.ts
+++ src/app/bciancio-pizza-toppings/bciancio-pizza-toppings.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BciancioPizzaToppingsComponent } from './bciancio-pizza-toppings.component';
 
 describe('BciancioPizzaToppingsComponent', () => {
-  let component: BciancioPizzaToppingsComponent;
-  let fixture: ComponentFixture<BciancioPizzaToppingsComponent>;
+	let component: BciancioPizzaToppingsComponent;
+	let fixture: ComponentFixture < BciancioPizzaToppingsComponent > ;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BciancioPizzaToppingsComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async (() => {
+		TestBed.configureTestingModule({
+				declarations: [BciancioPizzaToppingsComponent]
+			})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BciancioPizzaToppingsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(BciancioPizzaToppingsComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
